c5904fa2f0ee09a1de262a50dbef890cf055e3b0
Super95Git <50541407+Super95Git@users.noreply.github.com>
2022-04-05 17:09:07 +0530

    customized bot script
    customized bot script

--- js/Freshworks/freshworks_chatbot.js
+++ js/Freshworks/freshworks_chatbot.js
@@ -1,9 +1,7 @@
-<script>
-  function initFreshChat() {
+function initFreshChat() {
     window.fcWidget.init({
       token: "34295310-8e5d-42c9-8eaf-fdab3d2c6f3f",
       host: "https://wchat.in.freshchat.com"
     });
   }
   function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.in.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshdesk Messaging-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-</script>
