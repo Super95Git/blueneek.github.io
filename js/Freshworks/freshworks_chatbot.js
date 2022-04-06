906575e0e84e15f17677bb72a0cb0eb01d855aee
Super95Git <50541407+Super95Git@users.noreply.github.com>
2022-04-06 12:36:04 +0530

    version 6

--- js/Freshworks/freshworks_chatbot.js
+++ js/Freshworks/freshworks_chatbot.js
@@ -1,4 +1,3 @@
-<script>
   function initFreshChat() {
     window.fcWidget.init({
       token: "34295310-8e5d-42c9-8eaf-fdab3d2c6f3f",
@@ -6,4 +5,3 @@
     });
   }
   function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.in.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshdesk Messaging-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-</script>
